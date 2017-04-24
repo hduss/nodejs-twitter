0542ac7d1e3f744f49d32f38420709d780441357
gabriel <pour.blaireaux@yahoo.fr>
2017-04-24 11:22:21 +0200

    read param

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 // import
-var Twitter = require('twitter');
-var yaml = require('js-yaml');
-var fs   = require('fs');
+var config = require('config-yml');
+var ArgumentParser   = require('argparse').ArgumentParser;
 
-// configuration file
-var configPath = './config.test.yml';
+// init arguments parser
+var parser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp:true,
+    description: 'Argparse example'
+});
 
-// Get document, or throw exception on error
-try {
-    var doc = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
-    console.log(doc);
-} catch (e) {
-    console.log(e);
+// add parameter "keywords"
+parser.addArgument(
+    [ '-k', '--key-words' ],
+    {
+        help: 'tweeter key words'
+    }
+);
+
+// read args
+var args = parser.parseArgs();
+console.dir(args);
+
+// keywords argument is mandatory
+if( !args.key_words) {
+    console.warn('No keywords provided, please use -k + keyword1,keywords2');
+    process.exit(0);
 }
 
-var client = new Twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
-});
+// read keywords
+var keywords = args.key_words.split(',');
+
+// read configuration data
+var consumer_key = config.default.api.twitter.consumer_key;
+var consumer_secret= config.default.api.twitter.consumer_secret;
+var access_token_key= config.default.api.twitter.access_token_key;
+var access_token_secret= config.default.api.twitter.access_token_secret;
