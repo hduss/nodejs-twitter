b4db0bbd4e3d579e58c7cc60ea59d60f0af1778c
hduss <delmas.theo@laposte.net>
2017-04-24 11:38:43 +0200

    recup tweet OK

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ var fs = require('fs');
 
 
 t = new Twitter({
-    consumer_key: 'ezgfregregh',
-    consumer_secret: 'erqghhrthryjj',
-    token: 'rjtyrjtykjrszj',
-    token_secret: 'srtjtykjtykutyk'
-  });
 
+    consumer_key: 'eGCunHb3MgjLiqJ32vPyjzqDy',
+    consumer_secret: 's0xd6egFQDdE5jLURyBnoFHybDzxJKbZtbEXtk8Jey2JjSSleq',
+    token: '798784794150588416-jfqoFhOAYXrJglKZmJYPY3dhhYHPhmF',
+    token_secret: 'FGnxrqqDcMPukWMOEiSFNnpTK7ZHsdmXaq2F7UqCvjbfP'
+  });
 
 
 t.on('tweet', (tweet) => {
 
-  console.log(`Twet received -> ${tweet})`;
+  console.log(tweet);
 
 });
 
@@ -27,3 +27,10 @@ t.on('error', (err) => {
   console.log('Oh no');
 
 });
+
+
+
+
+t.track('nodejs');
+
+
