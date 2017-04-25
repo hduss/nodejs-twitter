e1b0a4e9871b5071350a9969b726e7cf4bab1c92
gabriel <pour.blaireaux@yahoo.fr>
2017-04-25 14:23:36 +0200

    ok

--- src/server/controller/MailController.js
+++ src/server/controller/MailController.js
@@ -3,13 +3,13 @@ const Mongoose = require('../../class/Mongoose');
 
 class MailController {
 
-    index(req, res) {
+    static index(req, res) {
 
     	const mongoose = Mongoose.getInstance();
 
     	mongoose.initDb();
 
-        const mail = mongoose.findDb( (err, mail) => {
+        mongoose.findDb( (err, mail) => {
 
             res.render('mails.ejs', { mail: mail });
 
