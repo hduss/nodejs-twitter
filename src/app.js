7829f692d59cd6b7ee0943b6ca0131b5c706ee24
gabriel <pour.blaireaux@yahoo.fr>
2017-04-25 10:31:56 +0200

    set server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const Server = require('./server/server');
+const Mongoose = require('./class/Mongoose');
 
-// create instance
+// create mongoose instance
+mongoose = new Mongoose();
+
+// create server instance
 server = new Server();
 
 // add route mail example
@@ -8,6 +12,7 @@ server.addRoute('/mails', (req, res) => {
    console.log('reached');
 });
 
-// start server
-server.start();
+// start mongoose
+mongoose.initDb( () => server.start() );
+
 
